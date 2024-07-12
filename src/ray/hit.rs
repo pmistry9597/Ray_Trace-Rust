c47f9a32f8feac9e15764f00c3099868dbe48b8a
Preet Mistry <pmistry9597@gmail.com>
2024-07-12 14:08:10 -0400

    improve speed of hit comparison + testing

--- src/ray/hit.rs
+++ src/ray/hit.rs
@@ -25,27 +25,28 @@ pub trait HasHitInfo : Hitable {
 // treat NAN same as inf so we can do total ordering on l as an f32
 impl<I> Ord for HitResult<I> {
     fn cmp(&self, other: &Self) -> Ordering {
-        // self.height.cmp(&other.height)
         let me = self.l; 
         let them = other.l;
-        
         let me_and_them = [me, them];
+        let neither_nan = me_and_them.iter().all(|e| !e.is_nan());
 
-        let both_nan = me_and_them.iter().all(|e| e.is_nan());
-        let one_inf = me_and_them.iter().any(|e| e.is_finite());
-        let one_nan = me_and_them.iter().any(|e| e.is_nan());
-        let inf_nan = one_inf && one_nan;
-
-        if both_nan || inf_nan {
-            Ordering::Equal
-        } else if one_nan {
-            if me.is_nan() {
-                Ordering::Greater
-            } else {
-                Ordering::Less
-            }
+        if neither_nan { // do this first! 99.9 percent of cases!
+            me.partial_cmp(&them).expect("But neither are nan!")
         } else {
-            me.partial_cmp(&them).expect("Above cases should handle this issue!")
+            let both_nan = me_and_them.iter().all(|e| e.is_nan());
+            let one_inf = me_and_them.iter().any(|e| e.is_infinite());
+            let one_nan = me_and_them.iter().any(|e| e.is_nan());
+            let inf_nan = one_inf && one_nan;
+
+            if both_nan || inf_nan {
+                Ordering::Equal
+            } else {
+                if me.is_nan() {
+                    Ordering::Greater
+                } else {
+                    Ordering::Less
+                }
+            }
         }
     }
 }
@@ -56,7 +57,61 @@ impl<I> PartialOrd for HitResult<I> {
 }
 impl<I> PartialEq for HitResult<I> {
     fn eq(&self, other: &Self) -> bool {
-        self.l == other.l
+        matches!(self.cmp(other), Ordering::Equal)
     }
 }
 impl<I> Eq for HitResult<I> {}
+
+
+#[cfg(test)]
+mod hit_result_test {
+    use super::*;
+
+    #[test]
+    fn test_less_than() {
+        let me = HitResult{l: -1.0, intermed: ()};
+        let them = HitResult{l: 1.0, intermed: ()};
+
+        assert!(me < them);
+    }
+    #[test]
+    fn test_greater_than() {
+        let me = HitResult{l: 1.0, intermed: ()};
+        let them = HitResult{l: -1.0, intermed: ()};
+
+        assert!(me > them);
+    }
+    #[test]
+    fn test_eq() {
+        let me = HitResult{l: 1.34324, intermed: ()};
+        let them = HitResult{l: 1.34324, intermed: ()};
+
+        assert!(me == them);
+    }
+    #[test]
+    fn test_nan_eq() {
+        let me = HitResult{l: f32::NAN, intermed: ()};
+        let them = HitResult{l: f32::NAN, intermed: ()};
+
+        assert!(me == them);
+    }
+    #[test]
+    fn test_inf_eq() {
+        let me = HitResult{l: f32::INFINITY, intermed: ()};
+        let them = HitResult{l: f32::INFINITY, intermed: ()};
+
+        assert!(me == them);
+    }
+    #[test]
+    fn test_nan_inf_eq() {
+        let me = HitResult{l: f32::NAN, intermed: ()};
+        let them = HitResult{l: f32::INFINITY, intermed: ()};
+
+        assert!(me == them);
+
+        let me = HitResult{l: f32::INFINITY, intermed: ()};
+        let them = HitResult{l: f32::NAN, intermed: ()};
+
+        assert!(me == them);
+    }
+}

f2711e9979412891b9369316eca95cecd2b930c5
Preet Mistry <pmistry9597@gmail.com>
2024-08-04 19:22:44 -0400

    rename

--- src/material/mod.rs
+++ src/material/mod.rs
@@ -1,8 +1,8 @@
-mod rgb_image;
+mod uv_image;
 mod interaction;
 mod dyn_diff_spec;
 mod uniform_diff_spec;
 
-pub use rgb_image::UVRgb32FImage;
+pub use uv_image::UVRgb32FImage;
 pub use dyn_diff_spec::DynDiffSpec;
 pub use uniform_diff_spec::*;

02142b8f91891a3242be2e0cb5f0b8270f84dcd5
Preet Mistry <pmistry9597@gmail.com>
2024-08-04 20:04:36 -0400

    shorten mesh extraction (preparing for full mesh loading woohoo)

--- src/builder/pr/mesh_from_node.rs
+++ src/builder/pr/mesh_from_node.rs
@@ -35,71 +35,77 @@ impl MeshFromNode {
         let poses: Vec<Vector3<f32>> = reader.read_positions().unwrap().map(|p| p.into()).collect();
         let poses: Vec<Vector3<f32>> = poses.iter().map(|v| v * self.uniform_scale ).collect();
 
-        let pbr_met_rough = primitive.material().pbr_metallic_roughness();
-        let tex_info = pbr_met_rough.base_color_texture().expect("no base color texture??");
-        let tex_data = &images[tex_info.texture().index()];
+        let material = primitive.material();
+        let pbr_met_rough = material.pbr_metallic_roughness();
+        
+        let (textures, tex_coords) = texinfo_to_uvtex_and_coords(&pbr_met_rough.base_color_texture(), &reader, &images);
+        let (normal_maps, norm_coords) = norminfo_to_uvtex_and_coords(&material.normal_texture(), &reader, &images);
 
-        let dyn_image = match tex_data.format {
-            gltf::image::Format::R8G8B8 => DynamicImage::ImageRgb8(
-                ImageBuffer::from_raw(tex_data.width, tex_data.height, tex_data.pixels.clone()).expect("doesn't fit??")
-            ),
-            _ => { panic!("different image format??"); },
-        };
-        let tex_image = dyn_image.to_rgb32f();
+        let tangents: Option<Vec<[f32; 3]>> = reader.read_tangents().map(|tans| tans.map(|t| t[..3].try_into().unwrap()).collect());
 
-        let norm_info = primitive.material().normal_texture().expect("no normal map texture??");
-        let norm_data = &images[norm_info.texture().index()];
-        let dyn_norm = match norm_data.format {
-            gltf::image::Format::R8G8B8 => DynamicImage::ImageRgb8(
-                ImageBuffer::from_raw(norm_data.width, norm_data.height, norm_data.pixels.clone()).expect("doesn't fit??")
-            ),
-            _ => { panic!("different image format??"); },
-        };
-        let norm_image = dyn_norm.to_rgb32f();
-
-        let tex_coords: Vec<Vector2<f32>> = reader.read_tex_coords(tex_info.tex_coord()).expect("no texture coordinates?").into_f32().map(|p| p.into()).collect();
-        let norm_coords: Vec<Vector2<f32>> = reader.read_tex_coords(norm_info.tex_coord()).expect("no normal coordinates?").into_f32().map(|p| p.into()).collect();
-        let tangents: Option<Vec<[f32; 3]>> = match reader.read_tangents() { 
-            Some(it) => Some(it.map(|t| t[..3].try_into().unwrap()).collect()),
-            None => None,
-        };
-
-        let (metal_rough_maps, mr_coords): (Vec<UVRgb32FImage>, Option<Vec<Vector2<f32>>>) = 
-        match pbr_met_rough.metallic_roughness_texture() {
-            Some(mr_texinfo) => {
-                let mr_coords: Vec<Vector2<f32>> = reader.read_tex_coords(mr_texinfo.tex_coord()).expect("no metal roughness map coordinates?").into_f32().map(|p| p.into()).collect();
-
-                let mr_data = &images[mr_texinfo.texture().index()];
-                let dyn_mr = match mr_data.format {
-                    gltf::image::Format::R8G8B8 => DynamicImage::ImageRgb8(
-                        ImageBuffer::from_raw(mr_data.width, mr_data.height, mr_data.pixels.clone()).expect("doesn't fit??")
-                    ),
-                    _ => { panic!("different image format??"); },
-                };
-                let mr_image = dyn_mr.to_rgb32f();
-
-                (vec![mr_image.into()], Some(mr_coords))
-            },
-            None => (vec![], None),
-        };
-
+        let (metal_rough_maps, mr_coords) = texinfo_to_uvtex_and_coords(&pbr_met_rough.metallic_roughness_texture(), &reader, &images);
         let metal_rough = PbrMetalRough {
             metal: pbr_met_rough.metallic_factor(),
             rough: pbr_met_rough.roughness_factor(),
             coords: mr_coords,
         };
+
         Mesh {
             poses,
             norms: reader.read_normals().unwrap().map(|p| p.into()).collect(),
             indices: flat_indices.chunks(3).map(|c| c.try_into().unwrap()).collect(),
-            tex_coords,
-            norm_coords,
-            tangents: match tangents { Some(t) => Some(t.iter().map(|ta| (*ta).into()).collect()), None => None }, 
+            tex_coords: tex_coords.unwrap(),
+            norm_coords: norm_coords.unwrap(),
+            tangents: tangents.map(|t| t.iter().map(|ta| (*ta).into()).collect()), 
             metal_rough,
             
-            textures: vec![tex_image.into()],
-            normal_maps: vec![norm_image.into()],
+            textures,
+            normal_maps,
             metal_rough_maps,
         }
     }
+}
+
+use gltf::texture::Info;
+use gltf::mesh::Reader;
+use gltf::image::Data;
+use gltf::material::NormalTexture;
+use gltf::{Buffer, Texture};
+
+fn norminfo_to_uvtex_and_coords<'a, 's, F>(norm_info: &Option<NormalTexture>, reader: &Reader<'a, 's, F>, images: &Vec<Data>) -> (Vec<UVRgb32FImage>, Option<Vec<Vector2<f32>>>) 
+where
+    F: Clone + Fn(Buffer<'a>) -> Option<&'s [u8]>,
+{
+    match norm_info {
+        Some(info) => get_uvtex_and_coords(&info.texture(), info.tex_coord(), reader, images),
+        None => (vec![], None),
+    }
+}
+
+fn texinfo_to_uvtex_and_coords<'a, 's, F>(tex_info: &Option<Info>, reader: &Reader<'a, 's, F>, images: &Vec<Data>) -> (Vec<UVRgb32FImage>, Option<Vec<Vector2<f32>>>) 
+where
+    F: Clone + Fn(Buffer<'a>) -> Option<&'s [u8]>,
+{
+    match tex_info {
+        Some(info) => get_uvtex_and_coords(&info.texture(), info.tex_coord(), reader, images),
+        None => (vec![], None),
+    }
+}
+
+fn get_uvtex_and_coords<'a, 's, F>(texture: &Texture, tex_coord: u32, reader: &Reader<'a, 's, F>, images: &Vec<Data>) -> (Vec<UVRgb32FImage>, Option<Vec<Vector2<f32>>>)
+where
+    F: Clone + Fn(Buffer<'a>) -> Option<&'s [u8]>,
+{
+    let coords: Vec<Vector2<f32>> = reader.read_tex_coords(tex_coord).expect("no metal roughness map coordinates?").into_f32().map(|p| p.into()).collect();
+
+    let image_data = &images[texture.index()];
+    let dyn_image = match image_data.format {
+        gltf::image::Format::R8G8B8 => DynamicImage::ImageRgb8(
+            ImageBuffer::from_raw(image_data.width, image_data.height, image_data.pixels.clone()).expect("doesn't fit??")
+        ),
+        _ => { panic!("different image format??"); },
+    };
+    let image = dyn_image.to_rgb32f();
+
+    (vec![image.into()], Some(coords))
 }
